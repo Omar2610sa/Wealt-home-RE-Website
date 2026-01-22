6d1b03373428cadc9451d8a9a7fc27ff0932203a
Omar2610sa <118198753+Omar2610sa@users.noreply.github.com>
2026-01-22 14:50:31 +0200

    feat: add real estate  Wealth hoem website

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const navBar = document.querySelector("[data-navbar]")
+const toggle = document.querySelector("[data-nav-toggle]")
+
+
+toggle.addEventListener("click", ()=>{
+    navBar.classList.toggle("active")
+}) 
+
+
+// Scroll Header
+const header = document.querySelector("[data-header]")
+window.addEventListener("scroll", ()=>{
+    header.classList.toggle("active", window.scrollY > 50? "add" : "remove"("active") )
+})
+
+// Fav-Icon active 
+const facIcon = document.querySelectorAll(".fav-btn")
+
+facIcon.forEach(facIcon => {
+    facIcon.addEventListener("click", ()=>{
+        facIcon.classList.toggle("active")
+    })
+})
+
+// Update year 
+
+const Year = document.querySelector(".year")
+const currentYear = new Date().getFullYear()
+Year.textContent = currentYear
