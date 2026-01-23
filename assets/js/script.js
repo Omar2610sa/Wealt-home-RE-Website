e222996b494d435bafa2c17a3bdd90d8916f5831
Omar2610sa <118198753+Omar2610sa@users.noreply.github.com>
2026-01-23 15:50:57 +0200

    Add: add dark mode feat

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,22 @@ const navBar = document.querySelector("[data-navbar]")
 const toggle = document.querySelector("[data-nav-toggle]")
 
 
-toggle.addEventListener("click", ()=>{
+toggle.addEventListener("click", () => {
     navBar.classList.toggle("active")
-}) 
+})
 
 
 // Scroll Header
 const header = document.querySelector("[data-header]")
-window.addEventListener("scroll", ()=>{
-    header.classList.toggle("active", window.scrollY > 50? "add" : "remove"("active") )
+window.addEventListener("scroll", () => {
+    header.classList.toggle("active", window.scrollY > 50 ? "add" : "remove"("active"))
 })
 
 // Fav-Icon active 
 const facIcon = document.querySelectorAll(".fav-btn")
 
 facIcon.forEach(facIcon => {
-    facIcon.addEventListener("click", ()=>{
+    facIcon.addEventListener("click", () => {
         facIcon.classList.toggle("active")
     })
 })
@@ -30,11 +30,25 @@ Year.textContent = currentYear
 
 // Scroll to top
 const scrollTopBtn = document.querySelector(".scroll-top-btn")
-window.addEventListener("scroll", ()=>{
-    scrollTopBtn.classList.toggle("active", window.scrollY > 200 ? "add" : "remove"("active"))
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 200) {
+        scrollTopBtn.classList.add("active")
+    } else {
+        scrollTopBtn.classList.remove("active")
+    }
 })
 
-scrollTopBtn.addEventListener("click", ()=>{
-    document.body.scrollTop = 0
-    document.documentElement.scrollTop = 0
+scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+
+//  Dark Mode Toggle
+const darkModeToggle = document.querySelector(".dark-mode-toggle")
+darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    darkModeToggle.classList.toggle("active")
 })
